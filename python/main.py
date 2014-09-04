cb1965123625980786efa5c0dccb2f8cb76e23ed
Maijin <Maijin@users.noreply.github.com>
2014-09-04 11:15:32 +0200

    Handle JSON in r2pipe-python

--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
 #/usr/bin/env python
 import pexpect
+import json
 
 class R2:
   def __init__(self, filename):
@@ -14,7 +15,12 @@ class R2:
     self._expect_eof_()
     return self.process.before
 
+  def cmd_json(self, cmd):
+    return json.loads(self.cmd(cmd))
+
 if __name__ == "__main__":
   r3 = R2("/bin/ls")
   print ("PI: "+r3.cmd("pi 5"))
   print (r3.cmd("pn"))
+  info = r3.cmd_json("ij")
+  print ("Architecture: " + info['bin']['machine'])
