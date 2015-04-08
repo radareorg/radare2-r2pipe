1cf75b26e65051700ca2518cc87084f0000effc1
Roi Martin <jroi.martin@gmail.com>
2015-04-09 01:34:30 +0200

    Support R2PIPE_{IN,OUT} in go's r2pipe

--- go/r2pipe/r2pipe.go
+++ go/r2pipe/r2pipe.go
@@ -9,7 +9,7 @@ look like the following snippet:
 	import (
 		"fmt"
 
-		r2pipe "github.com/radare/radare2-bindings/r2pipe/go"
+		"github.com/radare/radare2-bindings/r2pipe/go/r2pipe"
 	)
 
 	func main() {
@@ -34,9 +34,12 @@ package r2pipe
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
+	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 )
 
@@ -50,9 +53,43 @@ type Pipe struct {
 }
 
 // NewPipe returns a new r2 pipe and initializes an r2 core that will try to
-// load the provided file or URI. This pipe acts as a communication interface
-// between Go and r2.
+// load the provided file or URI. If file is an empty string, the env vars
+// R2PIPE_{IN,OUT} will be used as file descriptors for input and output, this
+// is the case when r2pipe is called within r2.
 func NewPipe(file string) (*Pipe, error) {
+	if file == "" {
+		return newPipeFd()
+	}
+	return newPipeCmd(file)
+}
+
+func newPipeFd() (*Pipe, error) {
+	r2pipeIn := os.Getenv("R2PIPE_IN")
+	r2pipeOut := os.Getenv("R2PIPE_OUT")
+	if r2pipeIn == "" || r2pipeOut == "" {
+		return nil, errors.New("missing R2PIPE_{IN,OUT} vars")
+	}
+	r2pipeInFd, err := strconv.Atoi(r2pipeIn)
+	if err != nil {
+		return nil, err
+	}
+	r2pipeOutFd, err := strconv.Atoi(r2pipeOut)
+	if err != nil {
+		return nil, err
+	}
+	stdout := os.NewFile(uintptr(r2pipeInFd), "R2PIPE_IN")
+	stdin := os.NewFile(uintptr(r2pipeOutFd), "R2PIPE_OUT")
+
+	r2p := &Pipe{
+		File:   "",
+		cmd:    nil,
+		stdin:  stdin,
+		stdout: stdout,
+	}
+	return r2p, nil
+}
+
+func newPipeCmd(file string) (*Pipe, error) {
 	cmd := exec.Command("r2", "-q0", file)
 	stdin, err := cmd.StdinPipe()
 	if err != nil {
@@ -62,7 +99,7 @@ func NewPipe(file string) (*Pipe, error) {
 	if err != nil {
 		return nil, err
 	}
-	if err := cmd.Start(); err != nil {
+	if err = cmd.Start(); err != nil {
 		return nil, err
 	}
 	// Read initial data
@@ -116,6 +153,9 @@ func (r2p *Pipe) Var(name string) (value string, err error) {
 
 // Close shuts down r2, closing the created pipe.
 func (r2p *Pipe) Close() error {
+	if r2p.File == "" {
+		return nil
+	}
 	if _, err := r2p.Run("q!"); err != nil {
 		return err
 	}
