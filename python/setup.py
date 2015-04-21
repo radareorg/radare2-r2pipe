816d59d6fd2d79d959b6c672a153ad21766f7ee0
Javier Collado <jcollado@nowsecure.com>
2015-04-21 13:56:37 +0200

    Use README.rst as long description

--- python/setup.py
+++ python/setup.py
@@ -1,11 +1,15 @@
 from distutils.core import setup
 import r2pipe
 
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
 setup(
     name='r2pipe',
     version=r2pipe.version(),
     license='MIT',
     description='Pipe interface for radare2',
+    long_description=readme,
     author='pancake',
     author_email='pancake@nopcode.org',
     url='http://rada.re',
