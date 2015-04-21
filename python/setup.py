58c1992e89c6ba09a482a9bd21a24d17953d4436
Javier Collado <jcollado@nowsecure.com>
2015-04-21 13:55:12 +0200

    Fix indentation

--- python/setup.py
+++ python/setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 import r2pipe
 
 setup (
-	name='r2pipe',
-	version=r2pipe.version(),
-	license='MIT',
-	description='Pipe interface for radare2',
-	author='pancake',
-	author_email='pancake@nopcode.org',
-	url='http://rada.re',
-	package_dir={'r2pipe': 'r2pipe'},
-	packages=['r2pipe']
+    name='r2pipe',
+    version=r2pipe.version(),
+    license='MIT',
+    description='Pipe interface for radare2',
+    author='pancake',
+    author_email='pancake@nopcode.org',
+    url='http://rada.re',
+    package_dir={'r2pipe': 'r2pipe'},
+    packages=['r2pipe']
 )
