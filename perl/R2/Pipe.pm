ce1f7ecefebec7a02a1333a891eb94aa95b08cc3
pancake <pancake@nopcode.org>
2015-05-01 02:10:05 +0200

    Add support for RLang for r2pipe-perl

--- perl/R2/Pipe.pm
+++ perl/R2/Pipe.pm
@@ -2,46 +2,54 @@
 
 use strict;
 use IPC::Open2;
+use IO::Handle;
 
 package R2::Pipe;
 
-my $file;
-my $fh;
-
 local $|=1;
-
-sub new {
-	my $class = shift;
-	$file = shift;
-	my $self = {};
-	bless $self, $class;
-	$self->_initialize ();
-	return $self;
-}
 
 my $Reader;
 my $Writer;
 my $pid;
+my $fh;
 
-sub _initialize {
-	$pid = IPC::Open2::open2 ($Reader, $Writer, "r2 -q0 $file") or die;
-	local $/="\0";
-	<$Reader>; #// read until null byte
+sub new {
+	my $class = shift;
+	my $file = shift;
+	my $self = {};
+	bless $self, $class;
+	use Env qw ($R2PIPE_IN $R2PIPE_OUT);
+	if ($R2PIPE_IN && $R2PIPE_OUT) {
+		$R2PIPE_IN = 0+$R2PIPE_IN;
+		$R2PIPE_OUT = 0+$R2PIPE_OUT;
+		open $Reader, "<&=", $R2PIPE_IN or die "cannot open reader pipe";
+		open $Writer, ">&=", $R2PIPE_OUT or die "cannot open writer pipe";
+		autoflush $Reader 1;
+		autoflush $Writer 1;
+		undef $pid;
+	} else {
+		$pid = IPC::Open2::open2 ($Reader, $Writer, "r2 -q0 $file") or die;
+		local $/="\0";
+		<$Reader>;
+	}
+	return $self;
 }
 
 sub cmd {
 	shift;
-	print $Writer (shift."\n");
+	my $cmd = shift;
+	print $Writer ("$cmd\n") or die("error");
 	local $/="\0";
-	return <$Reader>;
+	my $res = <$Reader>;
+	return $res;
 }
+
 sub quit {
 	cmd ("q!");
-
-	close $Reader;
-	close $Writer;
+	undef $Reader;
+	undef $Writer;
 	waitpid($pid, 0);
-	close $pid;
+	close $pid if $pid;
 }
 
 1;
