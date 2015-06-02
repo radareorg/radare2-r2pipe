0792557fa6a9cb19c758da3831c9c80eb44fad48
pancake <pancake@nopcode.org>
2015-06-02 22:18:49 +0200

    Initial working version of r2pipe.rs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,8 @@
 extern crate libc;
 use libc::{ c_void };
+use std::str;
 use std::env;
+use std::thread;
 
 
 #[allow(improper_ctypes)]
@@ -19,32 +21,28 @@ fn getenv(k: &str) -> i32 {
   }
 }
 
-fn main() {
-  let fd_in = getenv("R2PIPE_IN");
-  let fd_out = getenv("R2PIPE_OUT");
+  let fd_in = 9; //getenv("R2PIPE_IN");
+  let fd_out = 12; //getenv("R2PIPE_OUT");
 
   if fd_in == 0 || fd_out == 0 {
     println!("Run this from r2 plz");
     return;
   }
   unsafe {
-let mut data = [0;1024];
     let mut buf: [u8; 1024] = [0;1024];
     let cmd = "?e Hello World\n";
 
     let r = libc::write (fd_out, 
         cmd.as_ptr() as *const c_void,
         cmd.len() as u64);
+    let ret = libc::write (fd_out, "\x00".as_ptr() as *const c_void, 1);
 
-    libc::write (fd_out, "\x00".as_ptr() as *const c_void, 1);
-
-    println!("WR {} exp {}", r, cmd.len());
-    //libc::perror("libc::write".as_ptr() as *const i8);
-    
     let ret = libc::read (fd_in,
         buf.as_ptr() as *mut c_void,
         buf.len() as u64);
-    println!("RD {}\n", ret);
+
+    let s = std::str::from_utf8(&buf).unwrap();
+    println!("RD {} ->({})\n", ret, s);
   }
 /*
   let mut f = FileDesc::new(fd_in, true);
@@ -58,4 +56,6 @@ let mut data = [0;1024];
     libc::close (fd_in);
     libc::close (fd_out);
   }
+fn main() {
+  r2pipe::new
 }
