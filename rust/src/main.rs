2d6befe7bd39e7e925798123cbf7fbe56779cfd1
pancake <pancake@nopcode.org>
2015-06-03 09:06:10 +0200

    Initial working API for r2pipe.rs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,64 +1,20 @@
-extern crate libc;
-use libc::{ c_void };
-use std::str;
-use std::env;
-use std::thread;
-
-
-#[allow(improper_ctypes)]
-
-fn atoi(k: &str) -> i32 {
-  match k.parse::<i32>() {
-    Ok(val) => val,
-    Err(_) => 0
-  }
-}
-
-fn getenv(k: &str) -> i32 {
-  match env::var(k) {
-  Ok(val) => atoi(&val),
-  Err(_) => 0,
-  }
-}
-
-fn rlang() {
-  let fd_in = 9; //getenv("R2PIPE_IN");
-  let fd_out = 12; //getenv("R2PIPE_OUT");
-
-  if fd_in == 0 || fd_out == 0 {
-    println!("Run this from r2 plz");
-    return;
-  }
-  unsafe {
-    let mut buf: [u8; 1024] = [0;1024];
-    let cmd = "?e Hello World\n";
-
-    let r = libc::write (fd_out, 
-        cmd.as_ptr() as *const c_void,
-        cmd.len() as u64);
-    let ret = libc::write (fd_out, "\x00".as_ptr() as *const c_void, 1);
-
-    let ret = libc::read (fd_in,
-        buf.as_ptr() as *mut c_void,
-        buf.len() as u64);
-
-    let s = std::str::from_utf8(&buf).unwrap();
-    println!("RD {} ->({})\n", ret, s);
-  }
-/*
-  let mut f = FileDesc::new(fd_in, true);
-  let buf = "Hello, world!\n".as_bytes();
-  f.inner_write(buf);
-*/
-    println!("Using {} {} in rust!", fd_in, fd_out);
-
-  unsafe {
-    // -------- //
-    libc::close (fd_in);
-    libc::close (fd_out);
-  }
-}
+extern crate r2pipe;
+use r2pipe::R2Pipe;
 
 fn main() {
-  rlang();
+	if R2Pipe::in_session() {
+		let r2p = R2Pipe::open();
+		//println!("{}", r2p.cmd("?e Hello World"));
+		let json = r2p.cmdj("ij");
+		println!("{}", json.pretty());
+		println!("ARCH {}", json.find_path(&["bin","arch"]).unwrap());
+		r2p.close();
+	} else {
+		let mut r2p = R2Pipe::spawn("/bin/ls");
+		println!("{}", r2p.cmd("?e Hello World"));
+		let json = r2p.cmdj("ij");
+		println!("{}", json.pretty());
+		println!("ARCH {}", json.find_path(&["bin","arch"]).unwrap());
+		r2p.close();
+	}
 }
