e4ccc4369a3ceee3de6619bb33dfb48c36569d6c
Sushant <sushant.dinesh94@gmail.com>
2015-06-03 21:02:51 +0530

    Added Error Handling. Unified pipes

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,20 +1,12 @@
+#[macro_use]
 extern crate r2pipe;
 use r2pipe::R2Pipe;
 
 fn main() {
-	if let Some(_) = R2Pipe::in_session() {
-		let r2p = R2Pipe::open();
-		println!("{}", r2p.cmd("?e Hello World"));
-		let json = r2p.cmdj("ij");
-		println!("{}", json.pretty());
-		println!("ARCH {}", json.find_path(&["bin","arch"]).unwrap());
-		r2p.close();
-	} else {
-		let mut r2p = R2Pipe::spawn("/bin/ls");
-		println!("{}", r2p.cmd("?e Hello World"));
-		let json = r2p.cmdj("ij");
-		println!("{}", json.pretty());
-		println!("ARCH {}", json.find_path(&["bin","arch"]).unwrap());
-		r2p.close();
-	}
+	let mut r2p = open_pipe!("/bin/ls").unwrap();
+	println!("{}", r2p.cmd("?e Hello World"));
+	let json = r2p.cmdj("ij");
+	println!("{}", json.pretty());
+	println!("ARCH {}", json.find_path(&["bin","arch"]).unwrap());
+	r2p.close();
 }
