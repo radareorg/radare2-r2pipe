40fc6fea9030ed7f866a71f10951c99f4f7e4909
pancake <pancake@nopcode.org>
2015-06-11 21:56:26 +0200

    Move r2pipe.rs to https://github.com/radare/r2pipe.rs

--- rust/README.md
+++ rust/README.md
@@ -1,10 +1,1 @@
-r2pipe.rs
-=========
-
-The Rust Crate to interact with radare2.
-
-
-TODO
-----
-* Support async API
-* Support HTTP connectivity
+See https://github.com/radare/r2pipe.rs
--- rust/src/examples/json.rs
+++
@@ -1,8 +0,0 @@
-extern crate rustc_serialize;
-use rustc_serialize::json::Json;
-
-fn main() {
-	let contents = "{\"pop\":123}";
-	let data = Json::from_str(contents).unwrap();
-	println!("{}",data.pretty());
-}
