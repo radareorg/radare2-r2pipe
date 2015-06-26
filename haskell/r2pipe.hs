c0f086f7cd6d315b9e335acf6dcfa760d3514426
Avi Weinstock <aweinstock314@gmail.com>
2015-06-26 13:15:57 -0400

    Added HTTP support to the Haskell r2pipe bindings.

--- haskell/r2pipe.hs
+++ haskell/r2pipe.hs
@@ -1,15 +1,19 @@
 module R2pipe (R2Context(), open, cmd, cmdj) where
-import Data.Aeson
+import Data.Char
 import Data.Word
+import Network.HTTP
 import System.IO
 import System.Process
+import qualified Data.Aeson as JSON
 import qualified Data.ByteString.Lazy as L
 
 withPipes p = p { std_in = CreatePipe, std_out = CreatePipe, std_err = CreatePipe }
 
 createProcess' args = fmap f $ createProcess (withPipes args) where
     f (Just i, Just o, Just e, h) = (i, o, e, h)
-    f _ = error "Failed to open pipes to the subprocess."
+    f _ = error "createProcess': Failed to open pipes to the subprocess."
+
+stringToLBS = L.pack . map (fromIntegral . ord)
 
 lHTakeWhile :: (Word8 -> Bool) -> Handle -> IO L.ByteString
 lHTakeWhile p h = do
@@ -18,20 +22,19 @@ lHTakeWhile p h = do
         then fmap (c `L.cons`) $ lHTakeWhile p h
         else return L.empty
 
--- This ADT is intended to have branches for {HTTP, TCP, ...} in the future
-data R2Context = LocalCtx (Handle, Handle, Handle, ProcessHandle)
+data R2Context = HttpCtx String
+               | LocalCtx (Handle, Handle, Handle, ProcessHandle)
 
 open :: String -> IO R2Context
-open url = do
-    handles@(_, hOut, _, _) <- createProcess' $ proc "r2" ["-q0", url]
+open url@('h':'t':'t':'p':_) = return $ HttpCtx (url ++ "/cmd/")
+open filename = do
+    handles@(_, hOut, _, _) <- createProcess' $ proc "r2" ["-q0", filename]
     lHTakeWhile (/= 0) hOut -- drop the inital null that r2 emits
     return $ LocalCtx handles
 
 cmd :: R2Context -> String -> IO L.ByteString
-cmd (LocalCtx (hIn, hOut, _, _)) cmd = do
-    hPutStrLn hIn cmd
-    hFlush hIn
-    lHTakeWhile (/= 0) hOut
+cmd (HttpCtx url) cmd = fmap stringToLBS $ getResponseBody =<< simpleHTTP (getRequest (url ++ cmd))
+cmd (LocalCtx (hIn, hOut, _, _)) cmd = hPutStrLn hIn cmd >> hFlush hIn >> lHTakeWhile (/= 0) hOut
 
-cmdj :: R2Context -> String -> IO (Maybe Value)
-cmdj = (fmap decode .) . cmd
+cmdj :: R2Context -> String -> IO (Maybe JSON.Value)
+cmdj = (fmap JSON.decode .) . cmd
