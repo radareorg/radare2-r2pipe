6aa69f51a38b1b98bda15a4bfab2104be9846c1d
pancake <pancake@nopcode.org>
2015-07-27 17:31:24 +0200

    Initial import of the Swift r2pipe API

---
+++ swift/main.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+println("Hello r2pipe.swift!");
+
+let r2p = R2Pipe(url:"http://cloud.radare.org/cmd/");
+if (r2p != nil) {
+	r2p!.cmd ("pi 5 @ entry0", closure:{ (str:String)->() in
+		print (str)
+		exit (0);
+	});
+}
+
+/* main loop required for async network requests */
+NSRunLoop.currentRunLoop().run();
---
+++ swift/r2pipe.swift
@@ -0,0 +1,58 @@
+import Foundation
+
+enum R2PipeChannel {
+	case Unknown
+	case Http
+	case Spawn
+}
+
+extension String {
+	func URLEncodedString() -> String? {
+		var customAllowedSet =  NSCharacterSet.URLQueryAllowedCharacterSet()
+		var escapedString = self.stringByAddingPercentEncodingWithAllowedCharacters(customAllowedSet)
+		return escapedString
+	}
+	func ToR2WebURL(str:String) -> String {
+		var ret = self;
+		if !self.hasSuffix ("/") {
+			ret += "/";
+		}
+		return ret + str.URLEncodedString()!;
+	}
+}
+
+class R2Pipe {
+	var mode : R2PipeChannel = .Unknown;
+	var path = "";
+
+	init?(url: String) {
+		if (url.hasPrefix ("http://")) {
+			mode = .Http;
+			path = url;
+		} else {
+			return nil;
+		}
+	}
+
+	func cmdHttp(str: String, closure:(String)->()) -> Bool {
+		let urlstr = self.path.ToR2WebURL(str);
+		let url = NSURL(string: urlstr);
+		let request = NSURLRequest(URL: url!)
+		NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
+			let str = NSString(data: data, encoding: NSUTF8StringEncoding)
+			closure (str as! String);
+		}
+		return true;
+	}
+
+	func cmd(str:String, closure:(String)->()) -> Bool {
+		switch (mode) {
+		case .Http:
+			return cmdHttp(str, closure:closure);
+		case .Spawn:
+			return false;
+		default:
+			return false;
+		}
+	}
+}
