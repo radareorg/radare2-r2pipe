3e120afa1e9987e8fc41f22cff81e2182520276a
pancake <pancake@nopcode.org>
2015-09-08 12:05:50 +0200

    add esil syscall handler in python

---
+++ python/examples/syscall/int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import sys
+import r2pipe
+
+r2p = r2pipe.open()
+num = int(sys.argv[1])
+if num == 0x80:
+	r = r2p.cmdj("arj")
+	if r['eax'] == 1:
+		print "[SYSCALL EXIT] %d"%(r['ebx'])
+	elif r['eax'] == 4:
+		msg = r2p.cmd("psz %d@%d"%(r['edx'], r['ecx']))
+		print "[WRITE SYSCALL] ==> %s"%(msg)
+elif num == 3:
+	print "[INT3]"
+else:
+	print ("[unhandled SYSCALL %d]"%num)
---
+++ python/examples/syscall/int.r2
@@ -0,0 +1,9 @@
+e asm.arch=x86
+e asm.bits=32
+wx 43 b940000000 ba0c000000 31c040404040cd80cc
+w Hello World @ 0x40
+"e cmd.esil.intr=#!pipe python int.py"
+aei
+pd 10
+10aes
+ae*
