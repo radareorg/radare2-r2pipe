921e49f267432771c5f8d0eb90a9b1325169f0ee
pancake <pancake@nopcode.org>
2015-12-14 22:45:42 +0100

    Add libpgraph r2pipe.py example

---
+++ python/examples/libgraph.py
@@ -0,0 +1,53 @@
+import r2pipe
+import sys
+import os
+
+r2 = r2pipe.open("/bin/ls")
+libpath = ['', '.', '/lib', '/usr/lib']
+
+done = {}
+
+def findlib(lib):
+	if os.path.isfile(lib):
+		return lib
+	for a in libpath:
+		if os.path.isfile("%s/%s"%(a, lib)):
+			return "%s/%s"%(a, lib)
+	return []
+
+def getlibs(lib):
+	return r2.syscmdj("rabin2 -lj %s"%(lib))['libs']
+
+def filter(s):
+	return s.replace("-","_")
+
+def makeNode(name):
+	r2.cmd("agn %s"%(filter(name)))
+
+def makeEdge(f,t):
+	r2.cmd("age %s %s"%(filter(f), filter(t)))
+
+def graphlibs(src, root):
+	hs = src.replace("/","_")
+	hs = hs.replace("-","_")
+	try:
+		if done[hs]:
+			return;
+	except:
+		done[hs] = True
+	src = findlib(src)
+	makeNode(src)
+	for lib in getlibs(src):
+		lib = findlib(lib)
+		makeNode(lib)
+		makeEdge(src, lib)
+		graphlibs(lib, src)
+
+if len(sys.argv) > 1:
+	path = sys.argv[1]
+	graphlibs(path, None)
+	print r2.cmd("e scr.color=true;agg")
+	r2.quit()
+else:
+	print "Usage: libgraph.py [path-to-bin]"
+	sys.exit(1)
