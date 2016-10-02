b2a8b84d4eb879aa8f90dfb08d3d5948765860f2
pancake <pancake@nopcode.org>
2016-10-02 22:57:26 +0200

    Add JSON example for the Groovy test

--- java/examples/groovy/test.groovy
+++ java/examples/groovy/test.groovy
@@ -1,6 +1,19 @@
-import org.radare.r2pipe.*;
+/* Groovy.r2pipe hello world -- 2016 - pancake@nopcode.org */
 
-def r2 = new R2Pipe("/bin/ls");
+import org.radare.r2pipe.*;
+import groovy.json.JsonSlurper;
+import groovy.json.JsonOutput;
+
+class R2 {
+	def r2;
+	def cmd = { x -> r2.cmd(x).trim() }
+	def cmdj = { x -> new JsonSlurper().parseText(this.cmd(x)) }
+	def quit = { r2.quit() }
+}
+
+def r2 = new R2(r2: new R2Pipe("/bin/ls"))
 println r2.cmd("?V")
-println r2.cmd("pd 10")
+def obj = r2.cmdj("ij").bin
+println JsonOutput.prettyPrint(JsonOutput.toJson(obj))
+println ("Interpreter: " + obj.intrp);
 r2.quit()
