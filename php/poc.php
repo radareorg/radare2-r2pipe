c09ba272f2b56044ef71e9ca90c4aca1f176bd26
pancake <pancake@nopcode.org>
2016-10-08 01:27:43 +0200

    Initial PoC implementation of r2pipe.php

---
+++ php/poc.php
@@ -0,0 +1,31 @@
+<?php
+/* r2pipe.php PoC by pancake @ 2016 */
+
+$cwd = '.';
+$descs = array(
+	0 => array("pipe", "r"),  // stdin
+	1 => array("pipe", "w"),  // stdout
+	2 => array("pipe", "w")   // stderr
+);
+$proc = proc_open("r2 -q0 /bin/ls", $descs, $pipes);
+if (is_resource($proc)) {
+	 $msg = fread($pipes[1], 1);
+}
+
+function r2cmd($cmd) {
+	global $pipes;
+	$rc = fwrite($pipes[0], "$cmd\n");
+	return trim(fread($pipes[1], 4096));
+}
+
+function r2cmdj($cmd) {
+	return json_decode (r2cmd ($cmd), true);
+}
+
+print_r(r2cmdj("ij"));
+print(r2cmd("?V"));
+print(r2cmd("pd 10"));
+
+$return_value = proc_close($proc);
+
+?>
