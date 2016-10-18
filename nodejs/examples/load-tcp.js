52d83e338fd07db128f40705aba23321924f2a57
pancake <pancake@nopcode.org>
2016-10-18 18:11:21 +0200

    Use random port for load-tcp

--- nodejs/examples/load-tcp.js
+++ nodejs/examples/load-tcp.js
@@ -4,22 +4,42 @@ const r2pipe = require('..');
 const net = require('net');
 const fs = require('fs');
 
-const buf = fs.readFileSync('/bin/ls');
-const r2port = 9998;
 
-const server = net.createServer(function(client) {
+const buf = fs.readFileSync('/bin/ls');
+
+const server = net.createServer(client => {
   client.write(buf, null, _ => {
     client.destroy();
     server.close();
   });
 });
 
-server.listen(r2port);
+server.listen(0, _ => {
+  const port = server.address().port;
+  r2pipe.open('tcp://127.0.0.1:' + port, (err, r2) => {
+    if (err) {
+      throw err;
+    }
+    r2.cmd('pd 20', (err, res) => {
+      console.log(res);
+      r2.quit();
+    });
+  });
+});
 
-r2pipe.open('tcp://127.0.0.1:' + r2port, (err, r2) => {
+
+/*
+API design proposal:
+====================
+
+r2pipe.openBuffer(buf, (err, r2) => {
+  if (err) {
+    throw err;
+  }
   r2.cmd('pd 20', (err, res) => {
     console.log(res);
     r2.quit();
   });
 });
+*/
 
