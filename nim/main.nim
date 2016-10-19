e786c00c8ba424e44a3f401536bfc7c992a69833
pancake <pancake@nopcode.org>
2016-10-19 03:04:16 +0200

    Add R2PipeApi for r2pipe.nim

--- nim/main.nim
+++ nim/main.nim
@@ -6,4 +6,11 @@ proc test {.async.} =
   let res = await r2p.cmd("?V")
   echo(res);
 
-waitFor test();
+proc testApi =
+  let r2p = R2PipeApi();
+  let res = r2p.cmd("pd 20")
+  echo(res);
+  echo(r2p.cmd("?e hello world"))
+
+testApi()
+waitFor test()
--- nim/r2pipe.nim
+++ nim/r2pipe.nim
@@ -1,9 +1,10 @@
 import sockets, strutils
 import httpclient
 import asyncdispatch
+import sequtils
 
 type
-  R2PipeHttp = object
+  R2PipeHttp = ref object
     url*:string
 
 proc cmd*(r: R2PipeHttp, c: string):Future[string] {.async.} =
@@ -11,3 +12,28 @@ proc cmd*(r: R2PipeHttp, c: string):Future[string] {.async.} =
   var resp = await client.request(r.url & "/cmd/" & c)
   return resp.body;
 
+# dll api
+
+proc r_core_new(): pointer {.importc, dynlib: "libr_core.dylib".}
+proc r_core_cmd_str(c: pointer, cmd: cstring): cstring {.importc, dynlib: "libr_core.dylib".}
+proc r_core_free(c: pointer): void {.importc, dynlib: "libr_core.dyilb".}
+
+proc toString(str: seq[char]): string =
+  result = newStringOfCap(len(str))
+  for ch in str:
+    add(result, ch)
+  return result
+
+proc fromString(str: string): seq[char] =
+  return toSeq(str.items)
+
+type R2PipeApi = ref object
+  lib: pointer
+
+# proc construct(this: R2PipeAPI) =
+#  this.lib = r_core_new();
+
+proc cmd*(this: R2PipeApi, c: string): string =
+  if this.lib == nil:
+    this.lib = r_core_new()
+  return $r_core_cmd_str(this.lib, c)
