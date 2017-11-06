39594260624f0b1599193650e99deca80d1ab0e7
pancake <pancake@nopcode.org>
2017-11-06 01:57:50 +0100

    Add support for R2PIPE_IN/OUT in Ruby

--- ruby/r2pipe.rb
+++ ruby/r2pipe.rb
@@ -8,37 +8,58 @@ require 'shellwords'
 
 # R2Pipe is an easy way to communicate with an r2 core through ruby
 class R2Pipe
-  def initialize(file)
+  def initialize(file = nil)
     @file = file
-    exec = "r2 -q0 #{Shellwords.shellescape file} 2>/dev/null"
-    PTY.spawn(exec) do |read, write, pid|
-      @read = read
-      @write = write
-      @pid = pid
-      @read.gets("\0")
+    if file == nil
+      fdIn = ENV['R2PIPE_IN'].to_i
+      fdOut = ENV['R2PIPE_OUT'].to_i
+      print "in: ", fdIn, "\nout: ", fdOut, "\n"
+      @read = IO.new(fdIn, 'r')
+      @write = IO.new(fdOut, 'w')
+      @pid = -1
+    else
+      exec = "r2 -q0 #{Shellwords.shellescape file} 2>/dev/null"
+      PTY.spawn(exec) do |read, write, pid|
+        @read = read
+        @write = write
+        @pid = pid
+        @read.gets("\0")
+      end
     end
   end
 
   def cmd(str)
     @write.print "#{str}\n"
+    @write.flush
     @read.gets("\0")[0..-2]
+  end
+
+  def cmdj(str)
+    json(cmd(str))
   end
 
   def quit
     cmd('q!')
-    ::Process.wait @pid
+    @read.close
+    @write.close
+    if @pid != -1
+      ::Process.wait @pid
+    end
   end
 
   def json(str)
-    JSON.parse str.sub("\n", '').sub("\r", '')
+    if str != nil
+      JSON.parse str.sub("\n", '').sub("\r", '')
+    end
   end
 end
 
 puts 'r2pipe ruby api demo'
 puts '===================='
-r2p = R2Pipe.new '/bin/ls'
+# r2p = R2Pipe.new '/bin/ls'
+r2p = R2Pipe.new
 puts r2p.cmd 'pi 5'
 puts r2p.cmd 'pij 1'
-puts r2p.json(r2p.cmd 'pij 1')
+puts r2p.cmdj 'pij 1'
 puts r2p.cmd 'px 64'
 r2p.quit
