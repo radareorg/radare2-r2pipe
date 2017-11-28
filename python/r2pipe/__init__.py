993d186022c304bab3960c745368fbed232b9632
Alex Kornitzer <alex.kornitzer@countercept.com>
2017-11-28 16:44:26 +0000

    python: cmdj strip \n

--- python/r2pipe/__init__.py
+++ python/r2pipe/__init__.py
@@ -272,7 +272,7 @@ class open:
                         Returns a Python object respresenting the parsed JSON
                 """
                 try:
-                        data = json.loads(self.cmd(cmd))
+                        data = json.loads(self.cmd(cmd).replace('\n', ''))
                 except (ValueError, KeyError, TypeError) as e:
                         sys.stderr.write("r2pipe.cmdj.Error: %s\n" % (e))
                         data = None
