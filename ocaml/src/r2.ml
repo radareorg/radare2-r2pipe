895bbc408776860aef3dff22f08bd883864e45c6
Anton Kochkov <xvilka@gmail.com>
2017-12-14 16:25:57 +0800

    OCaml: ignore stderr in r2pipe

--- ocaml/src/r2.ml
+++ ocaml/src/r2.ml
@@ -40,7 +40,7 @@ let open_file f_name =
         (out_r, out_w),
         (_, err_w) = Unix.(pipe (), pipe (), pipe ())
     in
-    let args = [|"r2"; "-q0"; f_name|] in
+    let args = [|"r2"; "-2"; "-q0"; f_name|] in
     let pid = Unix.create_process "r2" args ins_r out_w err_w in
     (* Get rid of the beginning \x00 *)
     ignore (Unix.read out_r (Bytes.create 1) 0 1);
