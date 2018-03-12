b21622b2c83a3d70481cc57cfdb33656da0745d5
Javier Redondo <jredondo43@gmail.com>
2018-03-12 21:03:42 +0100

    Add nodejs Async/Await example

---
+++ nodejs/r2pipe-promise/test-async-await.js
@@ -0,0 +1,14 @@
+const r2promise = require('./');
+
+const radare2FTW = async () => {
+  try {
+    const r2 = await r2promise.open('/bin/ls');
+    const msg = await r2.cmd('?E hello world');
+    console.log(msg);
+    return r2.quit();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+radare2FTW();
