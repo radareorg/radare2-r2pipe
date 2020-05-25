65987c58ee6ca54129657c16db3d470f74c2ae8a
Anton Kochkov <anton.kochkov@gmail.com>
2020-05-25 15:08:45 +0800

    Reformat code with black

--- python/dbgtest.py
+++ python/dbgtest.py
@@ -2,8 +2,8 @@
 
 import r2pipe
 
-r2=r2pipe.open("/bin/ls", ["-nd"])
-for a in range(1,10):
-	regs = r2.cmdj("drj")
-	print("0x%x  0x%x" % (regs['rip'],regs['rsp']))
-	r2.cmd("ds")
+r2 = r2pipe.open("/bin/ls", ["-nd"])
+for a in range(1, 10):
+    regs = r2.cmdj("drj")
+    print("0x%x  0x%x" % (regs["rip"], regs["rsp"]))
+    r2.cmd("ds")
