99e2cfbc21d4be4bb2d2e20f3d9be9ba2f981385
pancake <pancake@nopcode.org>
2022-06-04 23:42:12 +0200

    Run the new r2pipe tests

--- python/examples/test2.py
+++ python/examples/test2.py
@@ -1,8 +1,23 @@
 import r2pipe
 
 r2 = r2pipe.open("-")
-# r2.cmd("aa")
-hello = r2.cmd("?e hello")
+r2.cmd("aa")
+hello = r2.cmd("?e hello").strip()
+if hello != "hello":
+    exit(1)
 print(hello)
-hello = r2.cmd("?e world")
+world = r2.cmd("?e world").strip()
+if world != "world":
+    exit(1)
+print(world)
+r2.quit()
+
+r2 = r2pipe.open("-")
+hello = r2.cmd("?e hello").strip()
+if hello != "hello":
+    exit(1)
 print(hello)
+world = r2.cmd("?e world").strip()
+if world != "world":
+    exit(1)
+print(world)
