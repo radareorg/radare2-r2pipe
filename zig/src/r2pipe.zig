83c419d0abe897b590ba9f141b8abfb3df30bfcf
pancake <pancake@nopcode.org>
2022-09-06 12:39:36 +0200

    Remove unnecessary allocators from the zig implementation

--- zig/src/r2pipe.zig
+++ zig/src/r2pipe.zig
@@ -12,10 +12,9 @@ pub const R2PipeMode = enum {
 pub const R2Pipe = struct {
     fd_in: i32 = -1,
     fd_out: i32 = -1,
-    allocator: *const std.mem.Allocator,
 
     pub fn init() R2Pipe {
-        return R2Pipe{ .allocator = &std.heap.page_allocator };
+        return R2Pipe{};
     }
 
     pub fn deinit(r2: *R2Pipe) void {
@@ -28,9 +27,7 @@ pub const R2Pipe = struct {
             _ = try std.os.write(r2.fd_out, "\n");
             const res = try std.os.read(r2.fd_in, &foo);
             if (res > 0) {
-                var p = try r2.allocator.alloc(u8, @intCast(usize, res));
-                std.mem.copy(u8, p, foo[0..res]);
-                return p;
+                return foo[0..res];
             }
             return "";
         }
