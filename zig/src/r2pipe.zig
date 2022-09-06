d0a3bbf9ee720a90d57307d3a2b28f5571e04d51
pancake <pancake@nopcode.org>
2022-09-06 12:37:43 +0200

    Address review comments from #zig-help

--- zig/src/r2pipe.zig
+++ zig/src/r2pipe.zig
@@ -3,13 +3,10 @@ const fmt = std.fmt; // @import("std.fmt");
 const print = @import("std").debug.print;
 const testing = std.testing;
 
-extern fn read(fd: i32, ptr: *[4096]u8, len: i32) i32;
-extern fn write(fd: i32, ptr: *const u8, len: i32) i32;
-
 pub const R2PipeMode = enum {
     env,
-    // http,
     api,
+    // http,
 };
 
 pub const R2Pipe = struct {
@@ -21,21 +18,19 @@ pub const R2Pipe = struct {
         return R2Pipe{ .allocator = &std.heap.page_allocator };
     }
 
-    //pub fn deinit(r2: *R2Pipe) void {
-    // r2.arena.deinit();
-    //}
+    pub fn deinit(r2: *R2Pipe) void {
+        r2.arena.deinit();
+    }
     pub fn cmd(r2: R2Pipe, s: []const u8) ![]const u8 {
-        var foo: [4096]u8 = [_]u8{0} ** 4096;
+        var foo: [4096]u8 = undefined;
         if (r2.fd_in != -1 and r2.fd_out != -1) {
-            _ = write(r2.fd_out, @ptrCast(*const u8, &s[0]), @intCast(i32, s.len));
-            _ = write(r2.fd_out, @ptrCast(*const u8, "\n"), 1);
-            const res = read(r2.fd_in, &foo, foo.len);
+            _ = try std.os.write(r2.fd_out, s);
+            _ = try std.os.write(r2.fd_out, "\n");
+            const res = try std.os.read(r2.fd_in, &foo);
             if (res > 0) {
-                var p = try r2.allocator.alloc(u8, @intCast(usize, 4096 + res + 1));
-                std.mem.copy(u8, p, &foo);
-                p = try r2.allocator.realloc(p, @intCast(usize, res + 1));
-                var ss: []const u8 = p;
-                return ss;
+                var p = try r2.allocator.alloc(u8, @intCast(usize, res));
+                std.mem.copy(u8, p, foo[0..res]);
+                return p;
             }
             return "";
         }
@@ -52,37 +47,10 @@ pub fn open(file: []const u8) !R2Pipe {
     if (std.mem.eql(u8, file, "")) {
         var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
         defer arena.deinit();
-
-        const env_map = try arena.allocator().create(std.process.EnvMap);
-        env_map.* = try std.process.getEnvMap(arena.allocator());
-        defer env_map.deinit();
-
-        const sin = env_map.get("R2PIPE_IN") orelse "";
-        const sout = env_map.get("R2PIPE_OUT") orelse "";
-        var fail = false;
-        if (std.mem.eql(u8, sin, "")) {
-            fail = true;
-        }
-        if (std.mem.eql(u8, sout, "")) {
-            fail = true;
-        }
-        const iin = try std.fmt.parseInt(i32, sin, 0);
-        const iout = try std.fmt.parseInt(i32, sout, 0);
-        // print("in:{d} out:{d}\n", .{ iin, iout });
-        r2.fd_in = iin;
-        r2.fd_out = iout;
+        const env_map = try std.process.getEnvMap(arena.allocator());
+        r2.fd_in = try std.fmt.parseInt(i32, env_map.get("R2PIPE_IN") orelse "-1", 10);
+        r2.fd_out = try std.fmt.parseInt(i32, env_map.get("R2PIPE_OUT") orelse "-1", 10);
     }
-    // _ = r2.cmd("o /bin/ls");
     _ = file;
-    //    print("{s}", @TypeOf(r2));
     return r2;
-}
-
-/// deprecated
-// pub export uses C semantics
-pub export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
 }
