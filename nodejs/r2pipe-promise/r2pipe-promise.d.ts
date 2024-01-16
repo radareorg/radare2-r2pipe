f0d500d60be81ec9a814436714ce5053e3f1ced7
pancake <pancake@nopcode.org>
2024-01-16 15:21:45 +0100

    Update r2pipe-promise with missing callbacks

--- nodejs/r2pipe-promise/r2pipe-promise.d.ts
+++ nodejs/r2pipe-promise/r2pipe-promise.d.ts
@@ -3,6 +3,13 @@ export declare class R2Pipe {
   static open(target: string, options?: string[]): R2Pipe;
   cmd(command: string): Promise<string>;
   cmdj(command: string): Promise<CommandResponse>;
+  cmdAt(command: string, addr: number): Promise<CommandResponse>;
+  call(command: string): Promise<string>;
+  callj(command: string): Promise<CommandResponse>;
+  callAt(command: string, addr: number): Promise<CommandResponse>;
+  plugin(string, string): boolean;
+  unload(string, string): boolean;
+  log(string);
   quit(): Promise<void>;
 }
 
